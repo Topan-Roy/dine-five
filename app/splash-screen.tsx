53ffadd31bc67d9f3af81f41cfcf907672ca176e
mdalifkhandev <mdalifk2002@gmail.com>
2026-01-18 01:16:27 +0600

    project setup and project splash screen complite

---
+++ app/splash-screen.tsx
@@ -0,0 +1,34 @@
+import { Image } from "expo-image";
+import { router } from "expo-router";
+import React, { useEffect } from "react";
+import { ImageBackground, View } from "react-native";
+
+const SplashScreen = () => {
+  useEffect(() => {
+    const time = setTimeout(() => {
+      router.push("/(auth)/login");
+    }, 2000);
+
+    return clearTimeout(time);
+  }, []);
+  return (
+    <ImageBackground
+      source={require("@/assets/images/splash-screen.png")}
+      resizeMode="cover"
+      style={{ flex: 1, width: "100%", height: "100%" }}
+    >
+      <View className="flex-1 items-center justify-center">
+        <Image
+          source={require("@/assets/images/splash-logo.svg")}
+          contentFit="contain"
+          style={{
+            height: 320,
+            width: 320,
+          }}
+        />
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default SplashScreen;
